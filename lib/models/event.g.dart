4c87085a28ebf74780bd09cb14c70464f20cf408
kevin Lechardeur <kevin.lechardeur@etu.uca.fr>
2025-01-06 17:00:00 +0100

    mise à jour dépendance + calendar_controller.dart + calendar_page.dart + daily_calendar.dart

---
+++ lib/models/event.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'event.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class EventAdapter extends TypeAdapter<Event> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Event read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Event(
+      title: fields[0] as String,
+      startTime: fields[1] as DateTime,
+      endTime: fields[2] as DateTime,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Event obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.startTime)
+      ..writeByte(2)
+      ..write(obj.endTime);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is EventAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
